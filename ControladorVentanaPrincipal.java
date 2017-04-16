9b77e8c0f661ab29d12e74f8a44388783bf03469
JavierCR16 <javier.contrerasm16@gmail.com>
2017-04-16 11:42:20 -0600

    Add files via upload
    cambios interfaz

--- ControladorVentanaPrincipal.java
+++ ControladorVentanaPrincipal.java
@@ -1,15 +1,250 @@
 package Interfaz;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControladorVentanaPrincipal implements Initializable {
 
+    @FXML
+    javafx.scene.control.TextField cuadroIngresarUsuario;
+
+    @FXML
+    Button botonLoguear;
+
+    @FXML
+    Label colaboradorLogueado;
+
+    @FXML
+    TextField cuadroNombreNuevoColaborador;
+
+    @FXML
+    TextField cuadroApellidosNuevoColaborador;
+
+    @FXML
+    TextField cuadroCorreoNuevoColaborador;
+
+    @FXML
+    TextField cuadroUsuarioNuevoColaborador;
+
+    @FXML
+    TextField cuadroResidenciaNuevoColaborador;
+
+    @FXML
+    ComboBox cuadroPaisNuevoColaborador;
+
+    @FXML
+    ComboBox cuadroSexoNuevoColaborador;
+
+    @FXML
+    Button botonAgregarColaborador;
+
+    @FXML
+    ComboBox cuadroEnviarSolicitudColaboradores;
+
+    @FXML
+    TextField cuadroCorreoLogueado;
+
+    @FXML
+    TextField cuadroCorreoAmigo;
+
+    @FXML
+    Button botonEnviarSolicitud;
+
+    @FXML
+    Button botonRefrescarColaboradores;
+
+    @FXML
+    Button botonAceptarSolicitud;
+
+    @FXML
+    Button botonRechazarSolicitud;
+
+    @FXML
+    Button botonActualizarSolicitudes;
+
+    @FXML
+    TableView tablaSolicitudesAmistad;
+
+    @FXML
+    TextField cuadroNombreNuevoRestaurante;
+
+    @FXML
+    ComboBox cuadroCiudadNuevoRestaurante;
+
+    @FXML
+    ComboBox cuadroPaisNuevoRestaurante;
+
+    @FXML
+    TextArea cuadroDescripcionNuevoRestaurante;
+
+    @FXML
+    TextArea cuadroInstruccionesNuevoRestaurante;
+
+    @FXML
+    TextArea cuadroDireccionNuevoRestaurante;
+
+    @FXML
+    Button botonAceptarRestaurante;
+
+    @FXML
+    ComboBox cuadroEstablecimiento;
+
+    @FXML
+    ComboBox cuadroRangoPrecio;
+
+    @FXML
+    ComboBox cuadroTipoCocina;
+
+    @FXML
+    ComboBox cuadroTiempoComida;
+
+    @FXML
+    ComboBox cuadroRestriccionesDieteticas;
+
+    @FXML
+    ComboBox cuadroBuenoPara;
+
+    @FXML
+    Button botonRefrescarRestaurantesCaracteristicas;
+
+    @FXML
+    ComboBox cuadroRestaurantesDisponiblesCaracteristicas;
+
+    @FXML
+    ComboBox tipoCocinaMas;
+
+    @FXML
+    ComboBox tiempoComidaMas;
+
+    @FXML
+    ComboBox restriccionesDieteticasMas;
+
+    @FXML
+    ComboBox buenoParaMas;
+
+    @FXML
+    Button agregarMasTipoCocina;
+
+    @FXML
+    Button agregarMasTiempoComida;
+
+    @FXML
+    Button agregarMasRestriccionesDieteticas;
+
+    @FXML
+    Button agregarMasBuenoPara;
+
+    @FXML
+    ComboBox cuadroActualizarRestaurantes;
+
+    @FXML
+    TextField cuadroActualizarNombreRestaurante;
+
+    @FXML
+    ComboBox cuadroActualizarCiudadRestaurante;
+
+    @FXML
+    ComboBox cuadroActualizarPaisRestaurante;
+
+    @FXML
+    TextArea cuadroActualizarDescripcionRestaurante;
+
+    @FXML
+    TextArea cuadroActualizarInstruccionesRestaurante;
+
+    @FXML
+    TextArea cuadroActualizarDireccionRestaurante;
+
+    @FXML
+    ComboBox cuadroActualizarEstablecimiento;
+
+    @FXML
+    ComboBox cuadroActualizarRangoPrecio;
+
+    @FXML
+    ComboBox cuadroActualizarTipoCocina;
+
+    @FXML
+    ComboBox cuadroActualizarTiempoComida;
+
+    @FXML
+    ComboBox cuadroActualizarRestriccionesDieteticas;
+
+    @FXML
+    ComboBox cuadroActualizarBuenoPara;
+
+    @FXML
+    Button botonActualizarRestaurante;
+
+    @FXML
+    Button botonRefrescarRestaurantesPlatillos;
+
+    @FXML
+    ComboBox cuadroRestaurantesPlatillos;
+
+    @FXML
+    TextField cuadroNombreNuevoPlatillo;
+
+    @FXML
+    TextArea cuadroDescripcionNuevoPlatillo;
+
+    @FXML
+    Button botonAgregarPlatillo;
+
+    @FXML
+    ComboBox cuadroRestauranteEliminarPlatillo;
+
+    @FXML
+    ComboBox cuadroPlatilloEliminarPlatillo;
+
+    @FXML
+    Button botonEliminarPlatillo;
+
+    @FXML
+    ComboBox agregarComentarioRestaurante;
+
+    @FXML
+    ComboBox agregarComentarioAnno;
+
+    @FXML
+    ComboBox agregarComentarioMes;
+
+    @FXML
+    ComboBox agregarComentarioClasificacionCliente;
+
+    @FXML
+    ComboBox agregarComentarioValoracion;
+
+    @FXML
+    TextArea agregarComentarioOpinion;
+
+    @FXML
+    Button botonAgregarComentario;
+
+    @FXML
+    Button botonRefrescarRestaurantesComentarios;
+
+    @FXML
+    TableView tablaRestaurantesComentarios;
+
+    @FXML
+    Button botonAceptarRestauranteComentario;
+
+    @FXML
+    ComboBox cuadroPlatillosRestaurante;
+
+    @FXML
+    TextArea cuadroComentarioPlatillo;
+
+    @FXML
+    Button agregarComentarioPlatillo;
+
+
     public void initialize(URL fxmlLocations, ResourceBundle resources){
-
-
 
     }
 
