ef17b83e186ac02b666aeb2d447c9d458a89deaf
rrdm23 <rrdm23998@hotmail.com>
2017-04-16 22:50:17 -0600

    Librería agregada :v

--- Interfaz/Main.java
+++ Interfaz/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //XdxdxdxdXdfgfg
+        //XdxdxdxdXdfgfg22
         Parent root = FXMLLoader.load(getClass().getResource("VentanaPrincipal.fxml"));
         primaryStage.setTitle("Restaurantes");
         primaryStage.setScene(new Scene(root, 1298, 597));
